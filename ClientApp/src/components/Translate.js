19420cddc410bae059761a75d8106f43c786887f
lsele <lselen@live.nl>
2022-04-21 23:25:33 +0200

    fix

--- ClientApp/src/components/Translate.js
+++ ClientApp/src/components/Translate.js
@@ -15,18 +15,6 @@ function Translate() {
         </p>
       </div>
       <div>
-        {/* <div class="dropdown-container">
-          <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-          </DropdownButton>
-          <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-          </DropdownButton>
-        </div> */}
         <Form>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>
