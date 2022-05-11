f019b89fcb086b6b7f9bb2a5a9bb487103a62f2e
Jingkit <1003967@hr.nl>
2022-05-11 09:51:49 +0200

    test123

--- ClientApp/src/components/screens/profilePage.js
+++ ClientApp/src/components/screens/profilePage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../design/profileCSS.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 function profilePage() {
 
     var url = new URL(window.location.href);
     var search_params = url.searchParams;
     url.search = search_params.toString();
-    
+    const profile = JSON.parse(window.localStorage.getItem("currentUser"));
+    const percentage = 100;
     return (
         <div className="box">
             <div className="Card">
@@ -18,7 +21,12 @@ function profilePage() {
                 <div className="lower-container">
                     <h3> user123 </h3>
                     <h4> Beginner </h4>
-                    <p>text</p>
+                    <p>Course level 1:
+                        <div style={{ width: 25, height: 25}}>
+                        <CircularProgressbar value={100} />
+                        </div>
+                    </p>
+                    <p>Course level 2: {profile.courseLevel2}</p>
                     <button>Sign out</button>
                 </div>
             </div>
