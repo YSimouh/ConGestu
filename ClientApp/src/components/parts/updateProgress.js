69aee98897a7eb470370a5f88bf1fd6d22089953
Jingkit <1003967@hr.nl>
2022-05-18 12:07:07 +0200

    update progress fix

--- ClientApp/src/components/parts/updateProgress.js
+++ ClientApp/src/components/parts/updateProgress.js
@@ -7,25 +7,25 @@ function updateProgress(level, answersCorrect) {
     currentUser = JSON.parse(currentUser);
 
     if (level === 1) {
-        const answers = answersCorrect > currentUser.courseLevel1.correctAnswers ? answersCorrect : currentUser.courseLevel1.correctAnswers;
+        const answers = answersCorrect > currentUser.courseLevel1.answersCorrect ? answersCorrect : currentUser.courseLevel1.answersCorrect;
         const key = currentUser.username;
         const user = {
             username: currentUser.username,
             password: currentUser.password,
-            courseLevel1: {totalAnswers: currentUser.courseLevel1.totalAnswers, correctAnswers: answers},
+            courseLevel1: {totalAnswers: currentUser.courseLevel1.totalAnswers, answersCorrect: answers},
             courseLevel2: currentUser.courseLevel2
         };
         window.localStorage.setItem(key, JSON.stringify(user))
     }
 
-    if (level == 2) {
-        const answers = answersCorrect > currentUser.courseLevel2.correctAnswers ? answersCorrect : currentUser.courseLevel2.correctAnswers;
+    if (level === 2) {
+        const answers = answersCorrect > currentUser.courseLevel2.answersCorrect ? answersCorrect : currentUser.courseLevel2.answersCorrect;
         const key = currentUser.username;
         const user = {
             username: currentUser.username,
             password: currentUser.password,
             courseLevel1: currentUser.courseLevel1,
-            courseLevel2: { totalAnswers: currentUser.courseLevel2.totalAnswers, correctAnswers: answersCorrect }
+            courseLevel2: { totalAnswers: currentUser.courseLevel2.totalAnswers, answersCorrect: answers }
         };
         window.localStorage.setItem(key, JSON.stringify(user))
     }
