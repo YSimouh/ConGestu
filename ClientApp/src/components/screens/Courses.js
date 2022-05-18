59d7662947a3cee6da2013deff703fa3781c3924
1005343 <70711640+1005343@users.noreply.github.com>
2022-05-18 12:29:26 +0200

    commit

--- ClientApp/src/components/screens/Courses.js
+++ ClientApp/src/components/screens/Courses.js
@@ -14,31 +14,34 @@ function Courses() {
 		return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 	  }
 	  
-	function generate(amount){
+	function generate(amount,basepath,signsarray,syntax){
 		const randquestions=[];
-		const signs=["A","B","C","D","E","F","G","H","O","Q","R","S","U","V","X","Y","Z"];
+		const signs=signsarray;
 		for(let index=0;index<amount;index++){
 			const correctanswer=getRandomInt(0,4);
-			const sign=getRandomInt(0,17);
+			const sign=getRandomInt(0,signsarray.length);
 			const path=""+signs[sign]
+			let alreadyassigned=[];
+			alreadyassigned+=path
+			console.log(alreadyassigned)
 			randquestions[index]={
 				answerOptions: [
 				],
-				picture:require("../pictures/sign/"+`${path}`+".png"),
+				picture:require("../pictures/"+`${basepath}`+"/"+`${path}`+".png"),
 			};
 			randquestions[index].answerOptions[correctanswer]={
-				answerText: 'Letter '+`${path}`+"",isCorrect:true
+				answerText: ""+`${syntax}`+" "+`${path}`+"",isCorrect:true
 			}
 			for(let b=0;b<4;b++){
 				if(b!=correctanswer){
-					let randint=getRandomInt(0,17)
+					let randint=getRandomInt(0,signsarray.length)
 					let randsign=""+signs[randint]
 					while(randsign==path){
-						randint=getRandomInt(0,17)
+						randint=getRandomInt(0,signsarray.length)
 						randsign=""+signs[randint]
 					}
 					randquestions[index].answerOptions[b]={
-						answerText:'Letter '+`${randsign}`+"",isCorrect:false
+						answerText:""+`${syntax}`+" "+`${randsign}`+"",isCorrect:false
 					}
 				}
 			}
@@ -49,8 +52,8 @@ function Courses() {
   return (
     <>
       <div class="rows">
-        <Cursusoption link={"\levelholder"} questions={generate(7)} header="Alfabet" text="Oefen je gebarentaal kunsten" class="item"/>
-        <Cursusoption link={"\Courses"} header="Activiteiten" text="Hoeveel weet jij over activiteiten? Test jezelf" class="item"/>
+        <Cursusoption link={"\levelholder"} questions={generate(7,"sign",["A","B","C","D","E","F","G","H","O","Q","R","S","U","V","X","Y","Z"],"Letter")} header="Alfabet" text="Oefen je gebarentaal kunsten" class="item"/>
+        <Cursusoption link={"\levelholder"} questions={generate(7,"2eten-en-drinken",["koekje","koffie","macaroni","mandarijn","melk","peer","sinaasappel"],"")} header="Activiteiten" text="Hoeveel weet jij over activiteiten? Test jezelf" class="item"/>
         <Cursusoption link={"\Courses"} header="Sporten" text="Sporten is voor iedereen belangrijk" class="item"/>
         
       </div>
