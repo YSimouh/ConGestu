898b14b93a8c8e43451eb427c32fcacecb0d5deb
1005343 <1005343@hr.nl>
2022-06-09 14:15:45 +0200

    oke

--- ClientApp/src/components/screens/Transcriber.js
+++ ClientApp/src/components/screens/Transcriber.js
@@ -1,58 +1,91 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'; 
+import {useState,useEffect} from 'react';
+import { Form, Button, Dropdown, DropdownButton, InputGroup } from 'react-bootstrap';
+import '../design/transcriber.css';
 import { MdKeyboardVoice,MdFiberManualRecord } from "react-icons/md";
 
-const SpraakHerkenning = window.SpeechRecognition || window.webkitSpeechRecognition;
-const mic = new SpraakHerkenning()
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const mic = new SpeechRecognition()
 
-mic.continuous=true
-mic.interimResults= true
-mic.lang='nl-NL'
+mic.continuous = true
+mic.interimResults = true
+mic.lang='nl-NL';
 
-export default function Transcriber(props) {
-    const [listening,setlistening]=useState(false)
-    const [message, setMessage] = useState('');
-    
-    const handleChange = event => {
-        setMessage(event.target.value);
-      };
+function transcriber() {
+  const [isListening, setIsListening] = useState(false)
+  const [note, setNote] = useState(null)
+  const [savedNotes, setSavedNotes] = useState([])
 
-      useEffect(()=>{
-        handlelisten()
-      },[listening])
-  
-      const handlelisten = ()=>{
-        if(listening){
-          mic.start()
-          mic.onend=()=>{
-            console.log('continue...')
-            mic.start
-          }
-        }
-        else{
-          mic.stop()
-          mic.onend = ()=>{
-            console.log('stopped')
-          }
-        }
-        mic.onstart=()=>{
-          console.log('Mics on')
-        }     
-        mic.onresult= event => {
-          const transcript= Array.from(event.results)
-            .map(result=>result[0])
-            .map(result=>result.transcript)
-            .join('')
-          setMessage(transcript)
-          mic.onerror=event=>{
-            console.log(event.error)
-          }
-        }
+  useEffect(() => {
+    handleListen()
+  }, [isListening])
+
+  const handleListen = () => {
+    if (isListening) {
+      mic.start()
+      mic.onend = () => {
+        console.log('continue..')
+        mic.start()
       }
+    } else {
+      mic.stop()
+      mic.onend = () => {
+        console.log('Stopped Mic on Click')
+      }
+    }
+    mic.onstart = () => {
+      console.log('Mics on')
+    }
+
+    mic.onresult = event => {
+      const transcript = Array.from(event.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+      console.log(transcript)
+      setNote(transcript)
+      mic.onerror = event => {
+        console.log(event.error)
+      }
+    }
+  }
+
+  const handleSaveNote = () => {
+    setSavedNotes([...savedNotes, note])
+    setNote('')
+  }
+
   return (
-    <div>
-              {!listening?<MdKeyboardVoice size={40} fill='black' onClick={()=>setlistening(!listening)}/>:<MdFiberManualRecord size={40} fill='black' onClick={()=>setlistening(!listening)}/>}
-              <p style={{ color: 'black' }}>{message}</p>
-    </div>
+    <>
+      <h1>Spraak naar tekst</h1>
+      <div className="container">
+        <div className='knoppen'>
+          <Button onClick={() => setIsListening(prevState => !prevState)}>
+            Opnemen
+          </Button>
+          <Button onClick={handleSaveNote} disabled={!note}>
+            Opslaan
+          </Button>
+
+        </div>
+        <div className="box">
+          <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <InputGroup>       
+            <Form.Control value={note} as="textarea" rows={4} />
+            </InputGroup>
+          </Form.Group>
+        </Form>
+        </div>
+        <div className="box">
+          <h2>Opnames</h2>
+          {savedNotes.map(n => (
+            <p key={n}>{n}</p>
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 
+export default transcriber
