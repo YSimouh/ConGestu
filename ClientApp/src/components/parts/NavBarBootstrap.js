c18fd06675bbc4352d60735f5886bcc5954fe999
yara098 <72077600+yara9990@users.noreply.github.com>
2022-06-13 12:57:31 +0200

    n

--- ClientApp/src/components/parts/NavBarBootstrap.js
+++ ClientApp/src/components/parts/NavBarBootstrap.js
@@ -1,30 +1,55 @@
 ﻿import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import '../design/NavBarBootstrap.css';
-
+import  logo from '../pictures/logo-transparant.png';
 export default function Navigation() {
+
     return (
         <>
         <div className='pos'>
             <Navbar className="NavBarBootstrap" collapseOnSelect fixed='top' expand='sm' >
+            {/* <h2 className='h2logo'>ConGestu</h2> */}
+            <img class="home-logo-img" src = {logo}/> 
                 <Container>
+                    
                     <Navbar.Toggle aria-controls='responsive-navbar-nav' />
                     <Navbar.Collapse id='responsive-navbar-nav'>
-                        <Nav>
-                            <Nav.Link href='/'>&nbsp;Home&nbsp;</Nav.Link>
-                            <Nav.Link href='/courses'>&nbsp;Cursus&nbsp;</Nav.Link>
-                            <Nav.Link href='/translate'>&nbsp;Vertaalmachine&nbsp;</Nav.Link>
-                            <Nav.Link href='/information'>&nbsp;Informatie&nbsp;</Nav.Link>
-                                <Nav.Link href='/login'>
-                                    <div className="image-container">
+                        <Nav >
+                            <Nav.Link href='/'><i class="fa-solid fa-house fa-xl"></i></Nav.Link>
+                            <Nav.Link href='/login'> <i class="fa-solid fa-user fa-xl"></i>
+                                    {/* <div className="logo-container" style={{ "margin-left": "10px", "margin-right": "10px" }}>
                                         <i class="fa-solid fa-user fa-xl"></i>
-                                    </div>
+                                    </div> */}
                                 </Nav.Link>
+                            <Nav.Link href='/courses'><i class="fa-solid fa-hands-asl-interpreting fa-xl"></i></Nav.Link>
+                            <Nav.Link href='/translate'><i class="fa-solid fa-language fa-xl"></i></Nav.Link>
+                            <Nav.Link href='/information'><i class="fa-solid fa-circle-info fa-xl"></i></Nav.Link>
+                                
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
             </div>
         </>
+        // <>
+        //     <div className="container">
+                  
+        //           <ul className="sidebar">
+        //           <li><span>Travelers</span></li>
+        //            <li><span><i className="fa fa-home"></i></span><span>&nbsp;Home&nbsp;</span></li>
+        //            <li><span><i className="fa fa-dashboard"></i></span><span>&nbsp;Cursus&nbsp;</span></li>
+        //            <li><span><i className="fa fa-users"></i></span><span>&nbsp;Vertaalmachine&nbsp;</span></li>
+        //            <li><span><i className="fa fa-shopping-cart"></i></span><span>&nbsp;Informatie&nbsp;</span></li>
+
+                   
+                   
+                   
+        //           </ul>
+                  
+        //           <div className="content">
+                     
+        //           </div>
+        //   </div>
+        // </>
     );
 }
