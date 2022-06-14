01a4b8e6fb0b3d7d975b236cb7f94c6640027a64
1005343 <1005343@hr.nl>
2022-06-14 13:35:58 +0200

    okay

--- ClientApp/src/components/parts/cursusmodules/2etenEnDrinken.js
+++
@@ -1,2 +0,0 @@
-// hier een component die de vragen toont voor de dingen in de catagorie eten en drinken
-// gebruikt de afbeeldingen in "src/components/pictures/2eten-en-drinken"
--- ClientApp/src/components/parts/cursusmodules/3sport.js
+++
@@ -1,2 +0,0 @@
-// hier de react component voor de vragen in de catagorie omgang
-// gebruikt de afbeeldingen "components/pictures/3omgang"
