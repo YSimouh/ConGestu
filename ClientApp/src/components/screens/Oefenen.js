f1a4585d1c2947173b7ddf587fa345cfdb461d42
1005343 <1005343@hr.nl>
2022-06-14 13:03:31 +0200

    button

--- ClientApp/src/components/screens/Oefenen.js
+++ ClientApp/src/components/screens/Oefenen.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import '../design/oefenen.css'
+import styles from '../design/buttondesign.css'
+import {AwesomeButton,AwesomeButtonProgress,AwesomeButtonSocial,} from 'react-awesome-button';
+import { Link } from 'react-router-dom';
 
 export default function Oefenen(props) {
   const location=useLocation();
-  const {path,signs,syntax,text}=location.state;
+  const {path,signs,syntax,text,webcam}=location.state;
+  let web=false;
+  if (typeof webcam !== 'undefined'){
+    web=true
+  }
   return (
     <div>
       <div className='oefenheadercontainer'>
         <div className='oefenheader'>
           <h1>Module oefenen</h1>
-          <p>
+          <p> 
             {text}
             Probeer eerst te raden wat het antwoord is, als je er niet uit komt kun je het antwoord onthullen. Dit doe je 
             door je muis over de foto te halen. Herhaal dit proces totdat je zeker weet dat je alles weet!
           </p>
         </div>
+          {webcam?
+          <Link to={{
+                     pathname:webcam
+                    }}>
+                    <AwesomeButton size='large' type="primary">Oefen met webcam</AwesomeButton>
+                    </Link>
+          :<></>}
+
       </div>
     <div className='oefencontainer'>
         {signs.map((sign) => 
